1fa78eb086c34cd517f0ed67991501654035ad66
charandass <charan1631@gmail.com>
2019-03-15 09:17:35 +0530

    update the shopping list

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
+var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
+var listItems = document.getElementsByTagName("li");
 
 function inputvalue() {
 		return input.value.length;
 }
+
 function createListElement() {
-	var li = document.createElement("li");
+		var li = document.createElement("li");
 		li.appendChild(document.createTextNode(input.value))
-		ul.appendChild(li);	
+		ul.appendChild(li);
 		input.value = "";
+
+		var btn = document.createElement("button");
+		btn.appendChild(document.createTextNode("Delete!"));
+		li.appendChild(btn);
+		btn.onclick = removeParent;
 }
 
 function addListAfterClick() {
@@ -24,4 +31,31 @@ function addListAfterEnter(event) {
 }
 button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress",addListAfterEnter)
+input.addEventListener("keypress",addListAfterEnter);
+
+ul.onclick=function(event){
+	var target=event.target;
+	target.classList.toggle("done");
+}
+
+function listLength(){
+	return listItems.length;
+}
+
+function deleteButton(){
+	var btn=document.createElement("button");
+btn.appendChild(document.createTextNode("Delete!"));
+listItems[i].appendChild(btn);
+btn.onclick=removeParent;
+}
+
+for( i=0;i<listLength();i++){
+
+deleteButton();
+}
+
+function removeParent(evt){
+	evt.target.parentNode.remove();
+} 
+
+
